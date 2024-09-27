d7609b23489e8917a80d3df9edd5f05097e83d12
Liliana Lessa <llessa.smo@eos-uptrade.de>
2024-09-27 14:13:42 +0100

    app - integrate Marketing into Container

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Marketing from './components/MarketingApp';
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return <h1>Container: Hi there!</h1>
+    return <div>
+        <h1>Container: Hi there!</h1>
+        <hr />
+        <MarketingApp />
+    </div>
 };
