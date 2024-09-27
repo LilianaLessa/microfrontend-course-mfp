76c120fe93c242ab0f37ef920ecff86b67b84bb7
Liliana Lessa <llessa.smo@eos-uptrade.de>
2024-09-27 13:21:22 +0100

    dashboard - landing and pricing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const mount = (element) => {
     ReactDOM.render(
-        <h1>Marketing: Hi there!</h1>,
+        <App />,
         element
     );
 };
